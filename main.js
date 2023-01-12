2522414083a2e9b08ca6790630939af07e499c19
Aidana777 <aidananiazalieva27@gmail.com>
2023-01-12 16:36:32 +0600

    calc problem

--- main.js
+++ main.js
@@ -6,31 +6,29 @@
 //   }
 //   deadline(request)
 // 3
-let numA = Number(prompt('Ввести 1 число'))
-let symbol= Number(prompt('Знак'))
-let numB = Number(prompt('Ввести 2 число'))
+let first = prompt("first number");
+let operator = prompt("operator");
+let second = prompt("second number");
 
 const calc = (a, b) => {
-    if(symbol=== '+'){
-        console.log(a + b + ' сложение')
-    } else if(symbol==-'-'){
-        console.log(a - b + ' вычитание')
-       }else if(symbol==-'*'){
-        console.log(a * b + ' умножение')
-       }else if(symbol==-'/'){
-        console.log(a / b + ' деление')
-       }else{
-        console.log('Error');
-       }
-   
-   
-  
-    
+    let total;
+    switch (operator) {
+        case '+':
+            console.log(a + b);
+            break;
+        case '-':
+            console.log(a - b);
+            break;
+        case '*':
+            console.log(a * b);
+            break;
+        case '/':
+            console.log(a / b);
+            break;
+    }
 }
-
-calc(numA,symbol, numB)
-
-const nums = (c,d)=>{
-    console.log(Math.min(7,22));
-}
-nums();
+calc(`${first} ${operator}${second}=${total}`)
+// const nums = (c,d)=>{
+//     console.log(Math.min(7,22));
+// }
+// nums();
