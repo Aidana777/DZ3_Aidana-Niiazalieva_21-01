f789fad322063d13abc70ca04489491e8388cfb8
Aidana777 <aidananiazalieva27@gmail.com>
2023-01-12 15:44:16 +0600

    finsh

--- main.js
+++ main.js
@@ -6,19 +6,29 @@
 //   }
 //   deadline(request)
 // 3
-// let numA = Number(prompt('Ввести 1 число'))
-// let numB = Number(prompt('Ввести 2 число'))
+let numA = Number(prompt('Ввести 1 число'))
+let symbol= Number(prompt('Знак'))
+let numB = Number(prompt('Ввести 2 число'))
 
-// const calc = (a, b) => {
-//     console.log(a + b + ' сложение')
-//     console.log(a - b + ' вычитание')
-//     console.log(a * b + ' умножение')
-//     console.log(a / b + ' деление')
-// }
+const calc = (a, b) => {
+    if(symbol=== '+'){
+        console.log(a + b + ' сложение')
+    } else if(symbol==-'-'){
+        console.log(a - b + ' вычитание')
+       }else if(symbol==-'*'){
+        console.log(a * b + ' умножение')
+       }else if(symbol==-'/'){
+        console.log(a / b + ' деление')
+       }else
+   
+   
+  
+    
+}
 
-// calc(numA, numB)
+calc(numA,symbol, numB)
 
 const nums = (c,d)=>{
-    console.log(Math.max(7,22));
+    console.log(Math.min(7,22));
 }
 nums();
