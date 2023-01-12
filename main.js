88ddb4f20c582bf832d4b051aa4160b7c2b3b77f
Aidana777 <aidananiazalieva27@gmail.com>
2023-01-12 15:26:22 +0600

    funcs

---
+++ main.js
@@ -0,0 +1,24 @@
+
+// 2
+// request = prompt('')
+//   function deadline(request) {
+//     console.log(request.length)
+//   }
+//   deadline(request)
+// 3
+// let numA = Number(prompt('Ввести 1 число'))
+// let numB = Number(prompt('Ввести 2 число'))
+
+// const calc = (a, b) => {
+//     console.log(a + b + ' сложение')
+//     console.log(a - b + ' вычитание')
+//     console.log(a * b + ' умножение')
+//     console.log(a / b + ' деление')
+// }
+
+// calc(numA, numB)
+
+const nums = (c,d)=>{
+    console.log(Math.max(7,22));
+}
+nums();
